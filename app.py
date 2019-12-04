24c7248a9c6688b1b3ef66f86f92a12a3827ae9f
Roger Rojas <rrojas@hyosystem.com>
2019-12-04 18:35:17 -0500

    Login Flask Parte 1

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager
+
 from forms import RegistroForm, PostForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+
+loginManager = LoginManager(app)
 
 posts = []
 
